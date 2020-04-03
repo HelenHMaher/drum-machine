073c9e86a4139fa1220602f6e9edc0204fe6b934
HelenHMaher <helenharlanmaher@gmail.com>
2020-04-03 11:07:48 +0200

    drum-pad-array with sounds

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const keyArr = [
+  {
+    keyNumber: 81,
+    keyLetter: "Q",
+    id: "bell-tower",
+    url:
+      "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Bells/859[kb]bell-tower.wav.mp3"
+  },
+  {
+    keyNumber: 87,
+    keyLetter: "W",
+    id: "bell-tree",
+    url:
+      "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Bells/353[kb]belltree.aif.mp3"
+  },
+  {
+    keyNumber: 69,
+    keyLetter: "W",
+    id: "buizen-bell-E",
+    url:
+      "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Bells/793[kb]buizen-bell-E.aif.mp3"
+  },
+  {
+    keyNumber: 65,
+    keyLetter: "A",
+    id: "cheesy-chimes",
+    url:
+      "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Bells/89[kb]cheesy-chimes.wav.mp3"
+  },
+  {
+    keyNumber: 83,
+    keyLetter: "S",
+    id: "deep-dissonant-bell",
+    url:
+      "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Bells/797[kb]deep_dissonant_bell.aif.mp3"
+  },
+  {
+    keyNumber: 68,
+    keyLetter: "D",
+    id: "hand-bells",
+    url:
+      "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Bells/806[kb]hand_bells.aif.mp3"
+  },
+  {
+    keyNumber: 90,
+    keyLetter: "Z",
+    id: "harmonic-bell",
+    url:
+      "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Bells/1079[kb]harmonic-bell-fx.wav.mp3"
+  },
+  {
+    keyNumber: 88,
+    keyLetter: "X",
+    id: "one-pretty-bell",
+    url:
+      "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Bells/1128[kb]one-pretty-bell.wav.mp3"
+  },
+  {
+    keyNumber: 67,
+    keyLetter: "C",
+    id: "one-round-bellhit",
+    url:
+      "https://sampleswap.org/samples-ghost/INSTRUMENTS%20(SINGLE%20SAMPLES)/Bells/782[kb]one-round-bellhit.wav.mp3"
+  }
+];
+
+function DrumPad() {
+  return (
+    <div className="drum-pad" onClick={this.playSound} id={this.props.clipID}>
+      <audio
+        className="sound"
+        id={this.props.keyLetter}
+        src={this.props.sound}
+      ></audio>
+      {this.props.keyLetter}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
+    <div id="drum-machine">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div> This is my drum machine!!!</div>
       </header>
+      <div id="display"></div>
+      <div id="drum-pad">
+        <DrumPad />
+      </div>
     </div>
   );
 }
