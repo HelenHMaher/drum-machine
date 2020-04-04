e13e67cbc2bbab1e8f13d253ff2c92fa7256053c
HelenHMaher <helenharlanmaher@gmail.com>
2020-04-04 10:12:12 +0200

    webpack scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import variables from "./_App.scss";
+import SingleDrumPad from "./SingleDrumPad";
 
 const keyArr = [
   {
@@ -67,14 +68,6 @@ const keyArr = [
   }
 ];
 
-const pressStyles = {
-  backgroundColor: variables.warning
-};
-
-const defaultStyles = {
-  backgroundColor: variables.darkMain
-};
-
 const offStyles = {
   color: variables.darkAccent
 };
@@ -102,63 +95,6 @@ const PowerButton = props => {
     );
   }
 };
-
-class SingleDrumPad extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      style: defaultStyles
-    };
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.playSound = this.playSound.bind(this);
-    this.styleChange = this.styleChange.bind(this);
-  }
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyPress);
-  }
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyPress);
-  }
-  handleKeyPress(event) {
-    if (event.keyCode === this.props.keyNumber) {
-      this.playSound();
-    }
-  }
-  styleChange() {
-    this.state.style === defaultStyles
-      ? this.setState({ style: pressStyles })
-      : this.setState({ style: defaultStyles });
-  }
-  playSound(event) {
-    if (this.props.power === true) {
-      const sound = document.getElementById(this.props.keyLetter);
-      sound.play();
-      this.props.updateDisplay(this.props.clipname);
-      this.styleChange();
-      setTimeout(() => this.styleChange(), 100);
-    } else {
-      this.props.updateDisplay("Turn Me On");
-      setTimeout(() => this.props.updateDisplay(""), 500);
-    }
-  }
-  render() {
-    return (
-      <div
-        className="drum-pad"
-        id={this.props.clipname}
-        onClick={this.playSound}
-        style={this.state.style}
-      >
-        {this.props.keyLetter}
-        <audio
-          src={this.props.url}
-          id={this.props.keyLetter}
-          className="clip"
-        ></audio>
-      </div>
-    );
-  }
-}
 
 class DrumPadSet extends React.Component {
   render() {
