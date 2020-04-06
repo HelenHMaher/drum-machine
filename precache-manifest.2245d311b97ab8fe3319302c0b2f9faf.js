6603cdbad328edc4c9545a15ec92b2e6090d6427
HelenHMaher <helenharlanmaher@gmail.com>
2020-04-06 14:39:01 +0200

    Updates

---
+++ precache-manifest.2245d311b97ab8fe3319302c0b2f9faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6556b1d869cc590923496cb78ab68184",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "0998baa4403720eee41e",
+    "url": "/drum-machine/static/css/main.1994d1b2.chunk.css"
+  },
+  {
+    "revision": "af46fd2d9ad35f32ebc3",
+    "url": "/drum-machine/static/js/2.c001a647.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.c001a647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0998baa4403720eee41e",
+    "url": "/drum-machine/static/js/main.d1e32466.chunk.js"
+  },
+  {
+    "revision": "315e3dcde1df1090667c",
+    "url": "/drum-machine/static/js/runtime-main.1958076e.js"
+  }
+]);
